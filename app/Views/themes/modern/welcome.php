516c2214fc0e9b18637cc5489b75ebbf1a5434fa
ekoerwiswandoko <ekoerwiswandoko@gmail.com>
2024-06-15 19:17:00 +0700

    update welcome page

--- app/Views/themes/modern/welcome.php
+++ app/Views/themes/modern/welcome.php
@@ -1,6 +1,6 @@
 <div class="card">
 	<div class="card-body">
-		<h4>Selamat datang di halaman administrasi</h4>
-		<p>Dihalaman ini Anda dapat melakukan berbagai pengaturan aplikasi sesuai dengan hak akses yang diberikan kepada Anda</p>
+		<h4>Selamat datang di halaman Welcome Page</h4>
+		<p>Dihalaman ini Anda dapat berbagai hal sesuai dengan kriteria anda</p>
 	</div>
 </div>
