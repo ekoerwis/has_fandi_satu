3ca7281885c06290c094935904ce58edbbc2dfe0
ekoerwiswandoko <ekoerwiswandoko@gmail.com>
2024-07-01 22:45:03 +0700

    front end skillbahasaview

---
+++ app/Models/Talent/SkillBahasaModel.php
@@ -0,0 +1,264 @@
+<?php
+
+namespace App\Models\Talent;
+// use CodeIgniter\Database\RawSql;
+
+class SkillBahasaModel extends \App\Models\BaseModel
+{
+	// private $fotoPath;
+	
+	public function __construct() {
+		parent::__construct();
+		// $this->fotoPath = 'public/images/foto/';
+	}
+
+	public function getMainSql($id='',$column='id_user'){
+        $mainSQL = "SELECT * FROM skill_bahasa where $column = '$id'";
+
+        $result = $this->db->query($mainSQL)->getResultArray();
+
+        return $result;
+    }
+
+    public function getRowMainSql($id='',$column='id_user'){
+
+        $sql = "select * from skill_bahasa where $column = '$id'";
+
+        $result = $this->db->query($sql)->getRowArray();
+
+        return $result;
+    }
+
+	public function getDataSqlByKodeBahasa($id='',$column='id_user', $kode_bahasa = 0){
+        $mainSQL = "SELECT * FROM skill_bahasa  where $column = '$id' and kode_bahasa = '$kode_bahasa' order by id";
+
+        $result = $this->db->query($mainSQL)->getResultArray();
+
+        return $result;
+    }
+
+    public function saveData($id_user='') 
+	{
+        $result = [];
+
+        $listDetailForm_anak=array();
+		$implodeListDetailForm_anak = "";
+
+        $listDetailForm_saudara=array();
+		$implodeListDetailForm_saudara = "";
+
+		$data_db['id_user'] = $id_user;
+        
+        $result['status']='error';
+        $result['message']='Proses gagal mohon ulangi kembali !';
+        $result['dismiss']=true;
+
+        if(isset($_POST['tanggal_lahir_ayah']) and isset($_POST['tanggal_lahir_ibu'])){
+            
+            // $data_db['tipe_keluarga']=[];
+            // $data_db['jenis_kelamin']=[];
+            // $data_db['pendidikan']=[];
+            // $data_db['profesi']=[];
+            // $data_db['tanggal_lahir']=[];
+
+            // ayah
+            $id=isset($_POST['id_ayah'])?intval($_POST['id_ayah']):0;
+            $data_db['tipe_keluarga']=1;
+            $data_db['jenis_kelamin']='';
+            $data_db['pendidikan']='';
+            $data_db['profesi']=isset($_POST['profesi_ayah'])?strval($_POST['profesi_ayah']):'';
+            
+            $exp = explode('-', $_POST['tanggal_lahir_ayah']);
+            $tgl_lahir = $exp[2].'-'.$exp[1].'-'.$exp[0];
+            $data_db['tanggal_lahir'] = isset($_POST['tanggal_lahir_ayah'])? $tgl_lahir :'';
+
+            if(empty($this->getRowMainSql($id,'id'))){
+                $save = $this->db->table('data_keluarga')->insert($data_db);
+                $result['status']='ok';
+                $result['message']='Data Berhasil Ditambah';
+                $result['dismiss']=false;
+            } else {
+                $update = $this->db->table('data_keluarga')->update($data_db, ['id' => $id]);
+                $result['status']='ok';
+                $result['message']='Data Berhasil Diubah';
+                $result['dismiss']=false;
+            }
+
+            // ibu
+            $id=isset($_POST['id_ibu'])?intval($_POST['id_ibu']):0;
+            $data_db['tipe_keluarga']=2;
+            $data_db['jenis_kelamin']='';
+            $data_db['pendidikan']='';
+            $data_db['profesi']=isset($_POST['profesi_ibu'])?strval($_POST['profesi_ibu']):'';
+            
+            $exp = explode('-', $_POST['tanggal_lahir_ibu']);
+            $tgl_lahir = $exp[2].'-'.$exp[1].'-'.$exp[0];
+            $data_db['tanggal_lahir'] = isset($_POST['tanggal_lahir_ibu'])? $tgl_lahir :'';
+
+            if(empty($this->getRowMainSql($id,'id'))){
+                $save = $this->db->table('data_keluarga')->insert($data_db);
+
+                $result['status']='ok';
+                $result['message']='Data Berhasil Ditambah';
+                $result['dismiss']=false;
+                
+            } else {
+                $update = $this->db->table('data_keluarga')->update($data_db, ['id' => $id]);
+                $result['status']='ok';
+                $result['message']='Data Berhasil Diubah';
+                $result['dismiss']=false; 
+            }
+
+            // pasangan
+            $id=isset($_POST['id_pasangan'])?intval($_POST['id_pasangan']):0;
+            $data_db['tipe_keluarga']=3;
+            $data_db['jenis_kelamin']='';
+            $data_db['pendidikan']='';
+            $data_db['profesi']=isset($_POST['profesi_pasangan'])?strval($_POST['profesi_pasangan']):'';
+            
+            $exp = explode('-', $_POST['tanggal_lahir_pasangan']);
+            $tgl_lahir = $exp[2].'-'.$exp[1].'-'.$exp[0];
+            $data_db['tanggal_lahir'] = isset($_POST['tanggal_lahir_pasangan'])? $tgl_lahir :'';
+
+            if(empty($this->getRowMainSql($id,'id'))){
+                $save = $this->db->table('data_keluarga')->insert($data_db);
+
+                $result['status']='ok';
+                $result['message']='Data Berhasil Ditambah';
+                $result['dismiss']=false;
+            } else {
+                $update = $this->db->table('data_keluarga')->update($data_db, ['id' => $id]);
+                $result['status']='ok';
+                $result['message']='Data Berhasil Diubah';
+                $result['dismiss']=false;
+            }
+
+
+            // anak
+            if(isset($_POST['tanggal_lahir_anak']) )
+                if($_POST['tanggal_lahir_anak'] != '')
+                    for($i=0 ; $i < count($this->request->getPost('tanggal_lahir_anak')) ; $i++ ){
+                        $id=isset($_POST['id_anak'][$i])?intval($_POST['id_anak'][$i]):0;
+                        $data_db['tipe_keluarga']=4;
+                        $data_db['jenis_kelamin']=isset($_POST['jenis_kelamin_anak'][$i])?strval($_POST['jenis_kelamin_anak'][$i]):'';
+                        $data_db['profesi']='';
+                        $data_db['pendidikan']=isset($_POST['pendidikan_anak'][$i])?strval($_POST['pendidikan_anak'][$i]):'';
+                        
+                        $exp = explode('-', $_POST['tanggal_lahir_anak'][$i]);
+                        $tgl_lahir = $exp[2].'-'.$exp[1].'-'.$exp[0];
+                        $data_db['tanggal_lahir'] = isset($_POST['tanggal_lahir_anak'][$i])? $tgl_lahir :'';
+
+                        if(empty($this->getRowMainSql($id,'id'))){
+                            $save = $this->db->table('data_keluarga')->insert($data_db);
+                            if($save){
+
+                                $sqlSearchSave = "select * from data_keluarga where tipe_keluarga = '".$data_db['tipe_keluarga']."'
+                                and jenis_kelamin = '".$data_db['jenis_kelamin']."'
+                                and pendidikan = '".$data_db['pendidikan']."'
+                                and profesi = '".$data_db['profesi']."'
+                                and tanggal_lahir = '".$data_db['tanggal_lahir']."'";
+
+                                $dataHasSave = $this->db->query($sqlSearchSave)->getRowArray();
+
+                                array_push($listDetailForm_anak,"'".$dataHasSave['id']."'");
+
+                                $result['status']='ok';
+                                $result['message']='Data Berhasil Ditambah';
+                                $result['dismiss']=false;
+                            } 
+                        } else {
+                            $update = $this->db->table('data_keluarga')->update($data_db, ['id' => $id]);
+                            if($update){
+                                $result['status']='ok';
+                                $result['message']='Data Berhasil Diubah';
+                                $result['dismiss']=false;
+                            } 
+                            array_push($listDetailForm_anak,"'".$id."'");
+                        }
+                        
+                    }
+                        
+                    if(count($listDetailForm_anak) > 0){
+                        $implodeListDetailForm_anak = implode(" , ",$listDetailForm_anak);
+                        $sqlDelete = "DELETE FROM data_keluarga WHERE id_user = '$id_user'  AND tipe_keluarga='4'  AND id NOT IN ($implodeListDetailForm_anak)";
+                        $delete = $this->db->query($sqlDelete);
+                    }
+
+            // anak
+            if(isset($_POST['tanggal_lahir_saudara']) )
+                if($_POST['tanggal_lahir_saudara'] != '')
+                    for($i=0 ; $i < count($this->request->getPost('tanggal_lahir_saudara')) ; $i++ ){
+                        $id=isset($_POST['id_saudara'][$i])?intval($_POST['id_saudara'][$i]):0;
+                        $data_db['tipe_keluarga']=5;
+                        $data_db['jenis_kelamin']=isset($_POST['jenis_kelamin_saudara'][$i])?strval($_POST['jenis_kelamin_saudara'][$i]):'';
+                        $data_db['profesi']=isset($_POST['profesi_saudara'][$i])?strval($_POST['profesi_saudara'][$i]):'';
+                        $data_db['pendidikan']='';
+                        
+                        $exp = explode('-', $_POST['tanggal_lahir_saudara'][$i]);
+                        $tgl_lahir = $exp[2].'-'.$exp[1].'-'.$exp[0];
+                        $data_db['tanggal_lahir'] = isset($_POST['tanggal_lahir_saudara'])? $tgl_lahir :'';
+
+                        if(empty($this->getRowMainSql($id,'id'))){
+                            $save = $this->db->table('data_keluarga')->insert($data_db);
+                            if($save){
+
+                                $sqlSearchSave = "select * from data_keluarga where tipe_keluarga = '".$data_db['tipe_keluarga']."'
+                                and jenis_kelamin = '".$data_db['jenis_kelamin']."'
+                                and pendidikan = '".$data_db['pendidikan']."'
+                                and profesi = '".$data_db['profesi']."'
+                                and tanggal_lahir = '".$data_db['tanggal_lahir']."'";
+
+                                $dataHasSave = $this->db->query($sqlSearchSave)->getRowArray();
+
+                                array_push($listDetailForm_saudara,"'".$dataHasSave['id']."'");
+
+                                $result['status']='ok';
+                                $result['message']='Data Berhasil Ditambah';
+                                $result['dismiss']=false;
+                            } 
+                        } else {
+                            $update = $this->db->table('data_keluarga')->update($data_db, ['id' => $id]);
+                            if($update){
+                                $result['status']='ok';
+                                $result['message']='Data Berhasil Diubah';
+                                $result['dismiss']=false;
+                            } 
+                            array_push($listDetailForm_saudara,"'".$id."'");
+                        }
+                        
+                    }
+                        
+                    if(count($listDetailForm_saudara) > 0){
+                        $implodeListDetailForm_saudara = implode(" , ",$listDetailForm_saudara);
+                        $sqlDelete = "DELETE FROM data_keluarga WHERE id_user = '$id_user'  AND tipe_keluarga='5' AND id NOT IN ($implodeListDetailForm_saudara)";
+                        $delete = $this->db->query($sqlDelete);
+                    }
+
+
+        } else {
+            $result['status']='error';
+            $result['message']='Proses gagal, Tidak Ada Data yang disimpan !';
+            $result['dismiss']=true;
+        }
+
+		return $result;
+
+	}
+    
+    public function getParameter($id='',$column='id_parameter'){
+
+        if(isset($_POST['id'])){
+
+            $id = isset($_POST['id']) ? strval($_POST['id']) :'';
+            $column = isset($_POST['column']) ? strval($_POST['column']) :'id_parameter';
+
+        }
+
+        $sql = "select * from parameter_detail where $column = '$id' order by sequence";
+
+        $result = $this->db->query($sql)->getResultArray();
+
+        return $result;
+    }
+}
+?>
---
+++ app/Views/Talent/SkillBahasa/SkillBahasaView.php
@@ -0,0 +1,219 @@
+<div class="card">
+	<div class="card-header bg-warning text-light">
+		<h5 class="card-title"><?=$title?></h5>
+	</div>
+	<div class="card-body">
+		
+		<?php
+
+		if (!empty($message)) {
+			show_message($message['message'], $message['status'], $message['dismiss']);
+		}
+		?>
+		<form method="post" action="" class="form-horizontal" enctype="multipart/form-data">
+        
+
+			<div class="tab-content" id="myTabContent">
+
+            <!-- bagian Jepang -->
+            <div class="card-header rounded mb-2 " style="padding-top: 15px; padding-bottom: 15px; background-color: #ffd191; ">
+                    <h5 class="">Bahasa Jepang</h5>
+                </div>
+				<div class="form-group col-sm-6">
+					<input class="form-control" type="text" name="id_jepang" value="<?= @$data_jepang[0]['id'] ?>" placeholder="id Jepang" readonly hidden/>
+					<label class="col-form-label">Jenis Sertifikasi</label>
+                    <select class="form-control jenis_sertifikat_jepang" name="jenis_sertifikat_jepang"  required="required">
+                        <option value="">Pilih Salah Satu</option>
+                    </select>
+				</div>
+				<div class="form-group  col-sm-6">
+                    <label class="col-form-label">Bulan Tahun Terbit</label>
+                    <div class="input-group">
+                        <select class="form-control bulan_terbit_jepang"  name="bulan_terbit_jepang">
+                            <option value="">Pilih Salah Satu</option>
+                        </select>
+                        <input type="number" class="form-control tahun_terbit_jepang"  value="<?= @$data_jepang['tahun_terbit'] ?>"  name="tahun_terbit_jepang" placeholder="Tahun Terbit">
+                    </div>
+				</div>
+                
+            <!-- bagian Inggris -->
+            <div class="card-header rounded mb-2 " style="padding-top: 15px; padding-bottom: 15px; background-color: #ffd191; ">
+                    <h5 class="">Bahasa Inggris</h5>
+                </div>
+				<div class="form-group col-sm-6">
+					<input class="form-control" type="text" name="id_inggris" value="<?= @$data_inggris[0]['id'] ?>" placeholder="id Inggris" readonly hidden/>
+                    <label class="col-form-label">Jenis Sertifikasi</label>
+                    <div class="input-group">
+                        <select class="form-control jenis_sertifikat_inggris" name="jenis_sertifikat_inggris"  required="required">
+                            <option value="">Pilih Salah Satu</option>
+                        </select>
+                        <input type="text" class="form-control level_inggris"  value="<?= @$data_inggris['level'] ?>"  name="level_inggris" placeholder="Nilai / Level">
+                    </div>
+				</div>
+				<div class="form-group  col-sm-6">
+                    <label class="col-form-label">Bulan Tahun Terbit</label>
+                    <div class="input-group">
+                        <select class="form-control bulan_terbit_inggris"  name="bulan_terbit_inggris">
+                            <option value="">Pilih Salah Satu</option>
+                        </select>
+                        <input type="number" class="form-control tahun_terbit_inggris"  value="<?= @$data_inggris['tahun_terbit'] ?>"  name="tahun_terbit_inggris" placeholder="Tahun Terbit">
+                    </div>
+				</div>
+
+                <!-- bagian Lainnya -->
+                <div class="card-header rounded mb-2 " style="padding-top: 15px; padding-bottom: 15px; background-color: #ffd191; ">
+                    <h5 class="">Lainnya</h5>
+                </div>
+                <div id="lain-container">
+                    <!-- Dynamic Lainnya input will be added here -->
+                </div>
+                <div class="col-sm-3 col-md-2 col-lg-3 col-xl-6 mt-3">
+                    <button type="button" class="btn btn-success" id="add-lain">Tambah Bahasa</button>
+                </div>
+
+
+                
+                <!--batas detail -->
+                
+                <div class="form-group row mb-0 mt-3">
+                    <div class="col-sm-5">
+						<button type="submit" name="submit" value="submit" class="btn btn-primary"><i class="far fa-save pr-2"></i>Save</button>
+					</div>
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
+
+
+<script>
+    $(document).ready(function(){
+
+
+        createOption('.jenis_sertifikat_jepang', '35', 'id_group',<?= @$data_jepang[0]['jenis_sertifikat'] ?>);
+        createOption('.bulan_terbit_jepang', '30', 'id_group',<?= @$data_jepang[0]['bulan_terbit'] ?>);
+
+        createOption('.jenis_sertifikat_inggris', '36', 'id_group');
+        createOption('.bulan_terbit_inggris', '30', 'id_group');
+
+
+        // Lain
+         // Add initial Lain section
+         var lainIndex = 0;
+
+        var data_lain_JSON = '<?php echo json_encode($data_lain); ?>';
+        var data_lain_Parse = JSON.parse(data_lain_JSON);
+
+        if(data_lain_Parse.length == 0){
+            lainIndex++;
+            addLainSection(lainIndex);
+            createOption('.bulan_terbit_lain-'+lainIndex, '30', 'id_group');
+        } else {
+            for (let i = 0; i < data_lain_Parse.length; i++) {
+                lainIndex++;
+                
+                addLainSection(lainIndex);
+                $('.id_saudara-'+lainIndex).val(data_lain_Parse[i]['id']) ;
+                $('.ket_bahasa_lain-'+lainIndex).val(data_lain_Parse[i]['ket_bahasa']) ;
+                $('.level_lain-'+lainIndex).val(data_lain_Parse[i]['level']) ;
+                $('.no_sertifikat_lain-'+lainIndex).val(data_lain_Parse[i]['no_sertifikat']) ;
+                $('.tahun_terbit_lain-'+lainIndex).val(data_lain_Parse[i]['tahun_terbit']) ;
+                $('.keterangan_lain-'+lainIndex).val(data_lain_Parse[i]['keterangan']) ;
+                
+                createOption('.bulan_terbit_lain-'+lainIndex, '30', 'id_group',data_lain_Parse[i]['bulan_terbit']);
+            }
+        }
+
+        // Add new lain section on button click
+        $('#add-lain').click(function() {
+            lainIndex++;
+            addLainSection(lainIndex);
+            createOption('.bulan_terbit_lain-'+lainIndex, '30', 'id_group');
+        });
+
+        function addLainSection(index_lain) {
+            var LainSection = `
+                <div class="form-group lain-section" data-index="${index_lain}">
+
+                <div class="input-group">
+                    <div class="form-group col-sm-6 ">
+					    <input class="form-control id_lain-${index_lain}" type="text" name="id_lain[]"  placeholder="id Lain" readonly hidden/>
+                        <label class="col-form-label">Bahasa</label>
+                        <input class="form-control ket_bahasa_lain-${index_lain}" type="text" name="ket_bahasa_lain[]" style="width:100%;" placeholder="Bahasa" />
+                    </div>
+
+                    <div class="form-group col-sm-6 ">
+                        <label class="col-form-label">Level</label>
+                        <input class="form-control level_lain-${index_lain}" type="text" name="level_lain[]" style="width:100%;" placeholder="Level / Nilai" />
+                    </div>
+                </div>
+
+                <div class="input-group">
+                    <div class="form-group col-sm-6 ">
+                        <label class="col-form-label">No Sertifikat</label>
+                        <input class="form-control no_sertifikat_lain-${index_lain}" type="text" name="no_sertifikat_lain[]" style="width:100%;" placeholder="No Sertifikat" />
+                    </div>
+
+                    <div class="form-group col-sm-6 ">
+                        <label class="col-form-label">Bulan Tahun Terbit</label>
+                        <div class="input-group">
+                            <select class="form-control bulan_terbit_lain-${index_lain}"  name="bulan_terbit_lain[]">
+                                <option value="">Pilih Salah Satu</option>
+                            </select>
+                            <input type="number" class="form-control tahun_terbit_lain-${index_lain}" name="tahun_terbit_lain[]" placeholder="Tahun Terbit">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="input-group">
+                    <div class="form-group col-sm-12 ">
+                        <label class="col-form-label">Keterangan</label>
+                        <input class="form-control keterangan_lain-${index_lain}" type="text" name="keterangan_lain[]" style="width:100%;" placeholder="Keterangan Lainnya" />
+                    </div>
+
+                </div>
+
+                
+                    
+                    <div class="col-sm-3 col-md-2 col-lg-3 col-xl-6 mt-3">
+                        <button type="button" class="btn btn-danger remove-lain">Hapus Bahasa</button>
+                    </div>
+                <hr class="bg-info" >
+                </div>
+            `;
+            $('#lain-container').append(LainSection);
+
+
+        }
+
+        // Remove child section on button click
+        $(document).on('click', '.remove-lain', function() {
+            $(this).closest('.lain-section').remove();
+        });
+
+        // batas lain
+
+
+});
+
+
+function createOption(id_input='', id_parametergroup='',column_parameter='id_parameter', defaultSelected =''){
+        $.ajax({
+            url: "<?= current_url().'/getParameter' ?>",
+            method: 'POST',
+            data:{
+                id : id_parametergroup ,
+                column : column_parameter
+            },
+            dataType: 'json',
+            success: function(response) {
+                var listOption = $(id_input);
+                response.forEach(function(responseData) {
+                    var selected = responseData.value_parameter == defaultSelected ? 'selected' : '';
+                    listOption.append('<option value="' + responseData.value_parameter + '" ' + selected + '>' + responseData.label_parameter + '</option>');
+                });
+            }
+        });
+    }
+
+</script>
